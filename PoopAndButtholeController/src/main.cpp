99c1666679982daebfee2690829b2b293539dd62
linmjie <linmgjie@gmail.com>
2026-01-20 17:01:18 -0500

    template for conveyor thingy

---
+++ PoopAndButtholeController/src/main.cpp
@@ -0,0 +1,64 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       deolin                                                    */
+/*    Created:      1/20/2026, 4:48:47 PM                                     */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+#include "vex.h"
+
+using namespace vex;
+
+// A global instance of vex::brain used for printing to the V5 brain screen
+vex::brain       Brain;
+controller Controller1 = controller(primary);
+
+//Main.. stuff
+//There's two pneumatics, don't know if this is right, there is a @class vex::pneumatics 
+cte::pneumatic pneumatic = cte::pneumatic(PORT1, true);
+
+#pragma region VEXcode Generated Robot Configuration
+// generating and setting random seed
+void initializeRandomSeed()
+{
+    int systemTime = Brain.Timer.systemHighResolution();
+    double batteryCurrent = Brain.Battery.current();
+    double batteryVoltage = Brain.Battery.voltage(voltageUnits::mV);
+
+    // Combine these values into a single integer
+    int seed = int(batteryVoltage + batteryCurrent * 100) + systemTime;
+
+    // Set the seed
+    srand(seed);
+}
+
+void vexcodeInit()
+{
+
+    // Initializing random seed.
+    initializeRandomSeed();
+}
+
+// Helper to make playing sounds from the V5 in VEXcode easier and
+// keeps the code cleaner by making it clear what is happening.
+void playVexcodeSound(const char *soundName)
+{
+    printf("VEXPlaySound:%s\n", soundName);
+    wait(5, msec);
+}
+#pragma endregion VEXcode Generated Robot Configuration
+
+// define your global instances of motors and other devices here
+
+
+int main() {
+
+    Brain.Screen.printAt( 10, 50, "Hello V5" );
+   
+    while(1) {
+        
+        // Allow other tasks to run
+        this_thread::sleep_for(10);
+    }
+}
