d6d4aaccca68a8aa0b22f92339161b05046e5177
linmjie <linmgjie@gmail.com>
2026-02-10 15:04:00 -0500

    idk stuff

--- PoopAndButtholeController/src/main.cpp
+++ PoopAndButtholeController/src/main.cpp
@@ -16,7 +16,8 @@ controller Controller1 = controller(primary);
 
 //Main.. stuff
 //There's two pneumatics, don't know if this is right, there is a @class vex::pneumatics 
-cte::pneumatic pneumatic = cte::pneumatic(PORT1, true);
+cte::pneumatic pneumatic = cte::pneumatic(Brain.ThreeWirePort.A, true);
+digital_in digIn = digital_in(Brain.ThreeWirePort.A);
 
 #pragma region VEXcode Generated Robot Configuration
 // generating and setting random seed
